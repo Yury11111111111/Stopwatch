1d77fd3e32102f675d2d82de7d4482c847d4b39c
unknown <yury.ovchinnkov@yandex.ru>
2022-05-26 12:28:21 +0300

    first commit

---
+++ src/Stopwatch.js
@@ -0,0 +1,50 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import "./Stopwatch.css";
+
+const Stopwatch = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [minuts, setMinuts] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  function funSwitch() {
+    setIsActive(!isActive);
+  }
+
+  function clear() {
+    setSeconds(0);
+    setMinuts(0);
+    setIsActive(false);
+  }
+
+  if (seconds === 60) {
+    setSeconds(0);
+    setMinuts((minuts) => minuts + 1);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0 && minuts !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds, minuts]);
+
+  return (
+    <div className="Stopwatch">
+      <div className="title">Секундомер</div>
+      <div className="timer">
+        {minuts} : {seconds}
+      </div>
+      <br />
+      <button className={`button button_${isActive ? 'active' : 'inactive'}`} onClick={funSwitch}>{isActive ? "Пауза" : "Старт"}</button>
+      <button className="clear" onClick={clear}>Очистить</button>
+    </div>
+  );
+};
+
+export default Stopwatch;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Stopwatch from './Stopwatch';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Stopwatch />
+);
