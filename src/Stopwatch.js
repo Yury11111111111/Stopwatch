62a3da8856fcbdfbcbb60d3225f109b29e53e94f
unknown <yury.ovchinnkov@yandex.ru>
2022-06-21 18:30:28 +0300

    second commit

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -23,11 +23,12 @@ const Stopwatch = () => {
   }
 
   useEffect(() => {
+    let date = Date.now()
     let interval = null;
     if (isActive) {
       interval = setInterval(() => {
         setSeconds((seconds) => seconds + 1);
-      }, 1000);
+      }, 980);
     } else if (!isActive && seconds !== 0 && minuts !== 0) {
       clearInterval(interval);
     }
